988e6bcbd283bc7602c02c66a6b20dc7e73b3a1e
charlotte <lottie.d.roman@gmail.com>
2021-02-26 10:12:11 +0000

    q learning single intersection

--- experiments/q_single-intersection.py
+++ experiments/q_single-intersection.py
@@ -18,30 +18,51 @@ from sumo_rl.environment.collect_p import CollectP2
 
 
 if __name__ == '__main__':
-
-    prs = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    prs = argparse.ArgumentParser(formatter_class=
+                                            argparse.ArgumentDefaultsHelpFormatter,
                                   description="""Q-Learning Single-Intersection""")
-    prs.add_argument("-route", dest="route", type=str, default='scenarios/mysingleintersection/single-intersection.rou.xml', help="Route definition xml file.\n")
-    prs.add_argument("-a", dest="alpha", type=float, default=0.1, required=False, help="Alpha learning rate.\n")
-    prs.add_argument("-g", dest="gamma", type=float, default=0.99, required=False, help="Gamma discount rate.\n")
-    prs.add_argument("-e", dest="epsilon", type=float, default=0.9, required=False, help="Epsilon.\n")
-    prs.add_argument("-me", dest="min_epsilon", type=float, default=0.005, required=False, help="Minimum epsilon.\n")
-    prs.add_argument("-d", dest="decay", type=float, default=0.99, required=False, help="Epsilon decay.\n")
-    prs.add_argument("-mingreen", dest="min_green", type=int, default=10, required=False, help="Minimum green time.\n")
-    prs.add_argument("-maxgreen", dest="max_green", type=int, default=30, required=False, help="Maximum green time.\n")
-    prs.add_argument("-gui", action="store_true", default=False, help="Run with visualization on SUMO.\n")
-    prs.add_argument("-fixed", action="store_true", default=False, help="Run with fixed timing traffic signals.\n")
-    prs.add_argument("-s", dest="seconds", type=int, default=100000, required=False, help="Number of simulation seconds.\n")
-    prs.add_argument("-r", dest="reward", type=str, default='wait2', required=False, help="Reward function: wait or wait2.\n")
-    prs.add_argument("-v", action="store_true", default=False, help="Print experience tuple.\n")
-    prs.add_argument("-runs", dest="runs", type=int, default=1, help="Number of runs.\n")
-    prs.add_argument("-tripfile", dest="tripfile", type=str, required=True, help="Choose a tripinfo output file name (.xml).\n")
-    prs.add_argument("-summaryfile", dest="summaryfile", default='outputs/summaryqsingle.xml', type=str, required=False, help="Choose a summary file name (.xml).\n")
+    prs.add_argument("-route", dest="route", type=str, 
+              default='scenarios/mysingleintersection/single-intersection.rou.xml', 
+                                               help="Route definition xml file.\n")
+    prs.add_argument("-a", dest="alpha", type=float, default=0.1, required=False,
+                                                     help="Alpha learning rate.\n")
+    prs.add_argument("-g", dest="gamma", type=float, default=0.99, required=False, 
+                                                     help="Gamma discount rate.\n")
+    prs.add_argument("-e", dest="epsilon", type=float, default=0.9, required=False, 
+                                                                 help="Epsilon.\n")
+    prs.add_argument("-me", dest="min_epsilon", type=float, default=0.005,
+                                        required=False, help="Minimum epsilon.\n")
+    prs.add_argument("-d", dest="decay", type=float, default=0.99, required=False, 
+                                                           help="Epsilon decay.\n")
+    prs.add_argument("-mingreen", dest="min_green", type=int, default=10, 
+                                      required=False, help="Minimum green time.\n")
+    prs.add_argument("-maxgreen", dest="max_green", type=int, default=30, 
+                                      required=False, help="Maximum green time.\n")
+    prs.add_argument("-gui", action="store_true", default=False, 
+                                          help="Run with visualization on SUMO.\n")
+    prs.add_argument("-fixed", action="store_true", default=False,
+                                   help="Run with fixed timing traffic signals.\n")
+    prs.add_argument("-s", dest="seconds", type=int, default=100000, 
+                            required=False, help="Number of simulation seconds.\n")
+    prs.add_argument("-r", dest="reward", type=str, default='wait2',
+                          required=False, help="Reward function: wait or wait2.\n")
+    prs.add_argument("-v", action="store_true", default=False, 
+                                                  help="Print experience tuple.\n")
+    prs.add_argument("-runs", dest="runs", type=int, default=1, 
+                                                          help="Number of runs.\n")
+    prs.add_argument("-tripfile", dest="tripfile", type=str, required=True, 
+                               help="Choose a tripinfo output file name (.xml).\n")
+    prs.add_argument("-summaryfile", dest="summaryfile", 
+                       default='outputs/summaryqsingle.xml', type=str, 
+                       required=False, help="Choose a summary file name (.xml).\n")
     args = prs.parse_args()
     experiment_time = str(datetime.now()).split('.')[0]
-    out_csv = 'outputs/my-single-intersection/q_{}_alpha{}_gamma{}_eps{}_decay{}_reward{}'.format(experiment_time, args.alpha, args.gamma, args.epsilon, args.decay, args.reward)
+    out_csv = 'outputs/my-single-intersection/q_{}_alpha{}_gamma{}_eps{}_decay{}'\
+    '_reward{}'.format(experiment_time, args.alpha, args.gamma, 
+                       args.epsilon, args.decay, args.reward)
 
-    env = SumoEnvironment(net_file='scenarios/mysingleintersection/single-intersection.net.xml',
+    env = SumoEnvironment(net_file='scenarios/mysingleintersection/'\
+                                    'single-intersection.net.xml',
                           route_file=args.route,
 			              trip_file=args.tripfile,
                           summary_file=args.summaryfile,
@@ -86,9 +107,13 @@ if __name__ == '__main__':
                                  action_space=env.action_space,
                                  alpha=args.alpha,
                                  gamma=args.gamma,
-                                 exploration_strategy=EpsilonGreedy(initial_epsilon=args.epsilon, min_epsilon=args.min_epsilon, decay=args.decay)) for ts in env.ts_ids}
+                                 exploration_strategy=EpsilonGreedy(
+                                        initial_epsilon=args.epsilon, 
+                                        min_epsilon=args.min_epsilon, 
+                                        decay=args.decay))for ts in env.ts_ids}
 
-        collect_ts_data =  CollectP2(ts_ids=env.ts_ids, phases=env.phases, filename='outputs/CollectingP/singleq') 
+        collect_ts_data =  CollectP2(ts_ids=env.ts_ids, phases=env.phases, 
+                                        filename='outputs/CollectingP/singleq') 
 
         done = {'__all__': False}
         infos = []
@@ -102,13 +127,17 @@ if __name__ == '__main__':
                 actions = {ts: ql_agents[ts].act() for ts in ql_agents.keys()}
 
                 s, r, done, _ = env.step(action=actions)
-                collect_ts_data._add_p_data(time=traci.simulation.getCurrentTime()/1000)
+                time = traci.simulation.getCurrentTime()/1000
+                collect_ts_data._add_p_data(time=time)
 
                 if args.v:
-                    print('s=', env.radix_decode(ql_agents['t'].state), 'a=', actions['t'], 's\'=', env.radix_encode(s['t']), 'r=', r['t'])
+                    print('s=', env.radix_decode(ql_agents['t'].state), 
+                          'a=', actions['t'], 's\'=', env.radix_encode(s['t']),
+                          'r=', r['t'])
 
                 for agent_id in ql_agents.keys():
-                    ql_agents[agent_id].learn(new_state=env.encode(s[agent_id]), reward=r[agent_id])
+                    ql_agents[agent_id].learn(new_state=env.encode(s[agent_id]),
+                                              reward=r[agent_id])
         collect_ts_data._write_p_data_file()
         env.save_csv(out_csv, run)
         env.close()
